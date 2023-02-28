f108ec13271135f7fe9bffcfe07086947e90a6b0
Daniela Garcia <93154412+DaaaaanG@users.noreply.github.com>
2023-02-27 19:14:31 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #C94B4B"> Desarrollo sitios web y estoy enculadísima de Moises Kempis')
+  .typeString('<span style="color: #C94B4B"> Desarrollo sitios web')
   .pauseFor(200)
   .deleteChars(10)
   .start();
